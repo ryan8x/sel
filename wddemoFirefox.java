6d4a65fa84cf1f34be08338b5aa75e05427fcd41
Liang <liangr@ca.metsci.com>
2017-12-13 17:57:54 -0800

    fix firefox test

--- wddemoFirefox.java
+++ wddemoFirefox.java
@@ -1,54 +1,34 @@
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class wddemoFirefox {
 
-	public static void main(String[] args) {
 
+	  	@Test
+		public void getArtistTest(){
+	  		
+			  System.setProperty("webdriver.gecko.driver", "C:\\selenium\\geckodriver.exe");	
+		      WebDriver driver = new FirefoxDriver();
 
-	      WebDriver driver = new FirefoxDriver();
+	          // Load sample calc test URL.
+		      driver.get("http://only-testing-blog.blogspot.com/2014/04/calc.html");
 
-	      //Puts an Implicit wait, Will wait for 10 seconds before throwing exception
-	      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
- 
-	      
-	      //Launch website
-	      driver.navigate().to("http://www.calculator.net/");
+		      // Execute sample calc test.
+		      driver.findElement(By.xpath("//input[@id='1']")).click();
+		           
+		      driver.findElement(By.xpath("//input[@id='plus']")).click();
+		   
+		      driver.findElement(By.xpath("//input[@id='6']")).click();
+		      driver.findElement(By.xpath("//input[@id='equals']")).click();
+		      String result = driver.findElement(By.xpath("//input[@id='Resultbox']")).getAttribute("value");
+		      System.out.println("Calc test result Is : " + result);
+		      driver.close();
 
-	      
-	      //Maximize the browser
-	      driver.manage().window().maximize();
-	      
-	      // Click on Math Calculators
-	      driver.findElement(By.xpath(".//*[@id='menu']/div[3]/a")).click();
-	      
-	      // Click on Percent Calculators
-	      driver.findElement(By.xpath(".//*[@id='menu']/div[4]/div[3]/a")).click();
-	      
-	      // Enter value 10 in the first number of the percent Calculator
-	      driver.findElement(By.id("cpar1")).sendKeys("10");
-	      
-	      // Enter value 50 in the second number of the percent Calculator
-	      driver.findElement(By.id("cpar2")).sendKeys("50");
-	      
-	      // Click Calculate Button
-	      driver.findElement(By.xpath(".//*[@id='content']/table/tbody/tr[2]/td/input[2]")).click();
-
-	      
-	      // Get the Result Text based on its xpath
-	      String result = driver.findElement(By.xpath(".//*[@id='content']/p[2]/font/b")).getText();
-
-	      
-	      // Print a Log In message to the screen
-	      System.out.println(" The Result is " + result);
-	      
-	      //Close the Browser.
-	      driver.close();
-  
-
-
+			Assert.assertEquals("7", result);
 	      
 	}
 
